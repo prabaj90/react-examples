466024d666f9b632845fda13be1b9bd3916148a4
prabaj90 <prabaj90@gmail.com>
2020-09-13 01:33:24 +0530

    UseEffect Examples

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Counter from './SetStateExample';
+import CounterWithName from './UseEffectExample';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+     <CounterWithName />
       <Counter />
     </div>
   );
--- src/SetStateExample/index.js
+++ src/SetStateExample/index.js
@@ -7,9 +7,9 @@ return(
     <div>
         <p>You have clicked {count} times</p>
         {count >= 10 ? <p>Counter reached maximum limit </p>:''}
-        <button onClick={() => count >= 10 ? setCount(count) : setCount(count + 1)}> Add Count</button>
-        <button onClick={() => count > 0 ? setCount(count - 1): setCount(count)}> Minus Count</button>
-        <button onClick={() => setCount(0)}> Reset Count </button>
+        <button onClick={() =>  setCount(count >= 10 ? count : count + 1)}> Add Count</button>
+        <button onClick={() =>  setCount(count > 0 ? count - 1 : count)}> Minus Count</button>
+        <button onClick={() =>  setCount(0)}> Reset Count </button>
     </div>
 );
 }
